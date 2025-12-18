c13159768b9e99e09dcfe89b8f4b4c2a8c22a020
bala-amypo <bala@amypo.in>
2025-12-18 14:54:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ColdRoom.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cold_rooms")
+public class ColdRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private Double minAllowed;
+    private Double maxAllowed;
+
+    public ColdRoom() {}
+
+    public ColdRoom(String name, String location, Double minAllowed, Double maxAllowed) {
+        this.name = name;
+        this.location = location;
+        this.minAllowed = minAllowed;
+        this.maxAllowed = maxAllowed;
+    }
+
+    // getters & setters
+}

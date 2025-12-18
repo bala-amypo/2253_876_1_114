c74ca230fefdae1620857d32e58d3e8eb22feaaa
bala-amypo <bala@amypo.in>
2025-12-18 16:40:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/QueueService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.QueuePosition;
+
+public interface QueueService {
+
+    QueuePosition updateQueuePosition(Long tokenId,
+                                      Integer newPosition);
+
+    QueuePosition getPosition(Long tokenId);
+}

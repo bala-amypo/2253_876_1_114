b2fe8dd6e60f3f88c117257fafcb8c0829a3bed6
bala-amypo <bala@amypo.in>
2025-12-19 04:18:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/TokenLogServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.BreachAlert;
+import com.example.demo.entity.TokenLog;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.TokenLogRepository;
+import com.example.demo.repository.TokenRepository;
+import com.example.demo.service.TokenLogService;
+
+@Service
+public class TokenLogServiceImpl implements TokenLogService {
+
+    private final TokenLogRepository logRepository;
+    private final TokenRepository tokenRepository;
+
+    public TokenLogServiceImpl(TokenLogRepository logRepository,
+                               TokenRepository tokenRepository) {
+        this.logRepository = logRepository;
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public TokenLog addLog(Long tokenId, String message) {
+
+        BreachAlert token = tokenRepository.findById(tokenId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Token not found"));
+
+        TokenLog log = new TokenLog(
+                token,
+                message,
+                LocalDateTime.now()
+        );
+
+        return logRepository.save(log);
+    }
+
+    @Override
+    public List<TokenLog> getLogs(Long tokenId) {
+
+        tokenRepository.findById(tokenId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Token not found"));
+
+        return logRepository.findByTokenIdOrderByLoggedAtAsc(tokenId);
+    }
+}

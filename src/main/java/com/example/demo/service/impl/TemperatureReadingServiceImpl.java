86a14ed8b49844866a698f4de4b4cd36fa4b8ae8
bala-amypo <bala@amypo.in>
2025-12-19 09:44:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureReadingServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureReadingServiceImpl.java
@@ -2,10 +2,15 @@ package com.example.demo.service.impl;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.ColdRoom;
+import com.example.demo.entity.SensorDevice;
 import com.example.demo.entity.TemperatureReading;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.ColdRoomRepository;
+import com.example.demo.repository.SensorRepository;
 import com.example.demo.repository.TemperatureReadingRepository;
 import com.example.demo.service.TemperatureReadingService;
 
@@ -13,18 +18,31 @@ import com.example.demo.service.TemperatureReadingService;
 public class TemperatureReadingServiceImpl implements TemperatureReadingService {
 
     private final TemperatureReadingRepository temperatureRepository;
+    private final SensorRepository sensorRepository;
+    private final ColdRoomRepository coldRoomRepository;
 
-    public TemperatureReadingServiceImpl(TemperatureReadingRepository temperatureRepository) {
+    public TemperatureReadingServiceImpl(
+            TemperatureReadingRepository temperatureRepository,
+            SensorRepository sensorRepository,
+            ColdRoomRepository coldRoomRepository) {
         this.temperatureRepository = temperatureRepository;
+        this.sensorRepository = sensorRepository;
+        this.coldRoomRepository = coldRoomRepository;
     }
 
     @Override
-    public TemperatureReading saveReading(String deviceId, Double value) {
+    public TemperatureReading saveReading(String sensorId, Double value) {
+
+        SensorDevice sensor = sensorRepository.findById(sensorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Sensor not found"));
+
+        ColdRoom coldRoom = sensor.getColdRoom();
 
         TemperatureReading reading = new TemperatureReading();
-        reading.setDeviceId(deviceId);
-        reading.setValue(value);
-        reading.setReadingTime(LocalDateTime.now());
+        reading.setSensor(sensor);
+        reading.setColdRoom(coldRoom);
+        reading.setReadingValue(value);
+        reading.setRecordedAt(LocalDateTime.now());
 
         return temperatureRepository.save(reading);
     }

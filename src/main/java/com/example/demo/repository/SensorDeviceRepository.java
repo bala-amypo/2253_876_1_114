f9750e0f970728e817a94f9182441e57253576fd
bala-amypo <bala@amypo.in>
2025-12-19 10:13:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorDeviceRepository.java
+++ src/main/java/com/example/demo/repository/SensorDeviceRepository.java
@@ -1,13 +1,7 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entity.SensorDevice;
 
-public interface SensorDeviceRepository
-        extends JpaRepository<SensorDevice, Long> {
-
-    Optional<SensorDevice> findByIdentifier(String identifier);
+public interface SensorDeviceRepository extends JpaRepository<SensorDevice, Long> {
 }

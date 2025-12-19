3ee8e0056769c2bed996fbeace1afc6124572724
bala-amypo <bala@amypo.in>
2025-12-19 08:17:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/QueuePositionRepository.java
+++ src/main/java/com/example/demo/repository/QueuePositionRepository.java
@@ -1,13 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.QueuePosition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.QueuePosition;
-
-public interface QueuePositionRepository
-        extends JpaRepository<QueuePosition, Long> {
+@Repository
+public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
 
     Optional<QueuePosition> findByTokenId(Long tokenId);
 }

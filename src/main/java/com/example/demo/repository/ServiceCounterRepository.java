d739262a171c17197841e278bc88f861c8c0f526
bala-amypo <bala@amypo.in>
2025-12-19 10:14:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceCounterRepository.java
+++ src/main/java/com/example/demo/repository/ServiceCounterRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.demo.entity.SensorDevice;
+import com.example.demo.entity.ServiceCounter;
 
 @Repository
-
+public interface ServiceCounterRepository extends JpaRepository<ServiceCounter, Long> {
+}

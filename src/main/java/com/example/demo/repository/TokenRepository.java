88f41ebaf608389f320247e85e6a16d88a91cb73
bala-amypo <bala@amypo.in>
2025-12-19 06:32:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/TokenRepository.java
+++ src/main/java/com/example/demo/repository/TokenRepository.java
@@ -1,13 +1,10 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
+import com.example.demo.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.BreachAlert;
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
 
-public interface TokenRepository
-        extends JpaRepository<BreachAlert, Long> {
-
-    Optional<BreachAlert> findByTokenNumber(String tokenNumber);
 }

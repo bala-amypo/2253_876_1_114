e9cb9b7f992649acbfc2f94c6ebdf5978fa23663
bala-amypo <bala@amypo.in>
2025-12-19 16:34:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureReadingServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureReadingServiceImpl.java
@@ -11,67 +11,48 @@ import com.example.demo.entity.TemperatureReading;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ColdRoomRepository;
 import com.example.demo.repository.SensorDeviceRepository;
-
-
-
 import com.example.demo.repository.TemperatureReadingRepository;
 import com.example.demo.service.TemperatureReadingService;
 
 @Service
 public class TemperatureReadingServiceImpl implements TemperatureReadingService {
 
-    private final TemperatureReadingRepository temperatureRepository;
+    private final TemperatureReadingRepository temperatureReadingRepository;
     private final SensorDeviceRepository sensorRepository;
- 
-
-
     private final ColdRoomRepository coldRoomRepository;
 
-    // public TemperatureReadingServiceImpl(
-    //         TemperatureReadingRepository temperatureRepository,
-    //         SensorRepository sensorRepository,
-    //         ColdRoomRepository coldRoomRepository) {
-    //     this.temperatureRepository = temperatureRepository;
-    //     this.sensorRepository = sensorRepository;
-    //     this.coldRoomRepository = coldRoomRepository;
-    // }
     public TemperatureReadingServiceImpl(
-        SensorDeviceRepository sensorRepository,
-        ColdRoomRepository coldRoomRepository,
-        TemperatureReadingRepository temperatureReadingRepository
-) {
-    this.sensorRepository = sensorRepository;
-    this.coldRoomRepository = coldRoomRepository;
-    this.temperatureReadingRepository = temperatureReadingRepository;
-}
+            TemperatureReadingRepository temperatureReadingRepository,
+            SensorDeviceRepository sensorRepository,
+            ColdRoomRepository coldRoomRepository) {
 
-
-    @Override
-    public TemperatureReading saveReading(String sensorId, Double value) {
-
-       SensorDevice sensor = sensorRepository.findById(Long.valueOf(deviceId))
-        .orElseThrow(() -> new ResourceNotFoundException("Sensor not found"));
-
-
-        ColdRoom coldRoom = sensor.getColdRoom();
-
-        TemperatureReading reading = new TemperatureReading();
-        reading.setSensor(sensor);
-        reading.setColdRoom(coldRoom);
-        reading.setReadingValue(value);
-        reading.setRecordedAt(LocalDateTime.now());
-
-        return temperatureRepository.save(reading);
+        this.temperatureReadingRepository = temperatureReadingRepository;
+        this.sensorRepository = sensorRepository;
+        this.coldRoomRepository = coldRoomRepository;
     }
 
     @Override
-    public TemperatureReading getReading(Long id) {
-        return temperatureRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Temperature Reading not found"));
+    public TemperatureReading saveReading(String deviceId, Double value) {
+
+        SensorDevice sensor = sensorRepository.findById(Long.valueOf(deviceId))
+                .orElseThrow(() -> new ResourceNotFoundException("Sensor not found"));
+
+        ColdRoom coldRoom = coldRoomRepository.findById(
+                        sensor.getColdRoom().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Cold room not found"));
+
+        TemperatureReading reading = new TemperatureReading(
+                sensor,
+                coldRoom,
+                value,
+                LocalDateTime.now()
+        );
+
+        return temperatureReadingRepository.save(reading);
     }
 
     @Override
-    public List<TemperatureReading> getAllReadings() {
-        return temperatureRepository.findAll();
+    public List<TemperatureReading> getReadings(Long sensorId) {
+        return temperatureReadingRepository.findBySensorId(sensorId);
     }
 }

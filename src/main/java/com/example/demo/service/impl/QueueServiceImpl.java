045a2a5b25b0d3e2aacca9f6606c6eadf25d8d0f
bala-amypo <bala@amypo.in>
2025-12-19 08:07:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/QueueServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/QueueServiceImpl.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.Token;
 import com.example.demo.entity.Queue;
-
 import com.example.demo.entity.QueuePosition;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.QueuePositionRepository;

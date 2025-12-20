0b9bd2d13eed1a774d8195f54801708349dbf49b
bala-amypo <bala@amypo.in>
2025-12-20 12:41:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/ColdRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ColdRoom;
-
-public interface ColdRoomRepository extends JpaRepository<ColdRoom, Long> {
-}

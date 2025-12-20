26797af3c75f304b5a4de5c9cb82021f2286dda5
bala-amypo <bala@amypo.in>
2025-12-20 19:56:24 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,21 +1,29 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Digital Queue Management System",
+                version = "1.0",
+                description = "API Documentation"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9023.pro604cr.amypo.ai"),
-                        new Server().url("http://9023.pro604cr.amypo.ai")
-                ));
-    }
 }

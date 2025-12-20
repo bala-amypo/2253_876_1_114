ade05056cb58900648345c769d8d7ac5971f0e42
bala-amypo <bala@amypo.in>
2025-12-20 18:33:34 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,24 +1,30 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "Digital Queue API", version = "1.0"),
-        security = {
-                @SecurityRequirement(name = "bearerAuth")
-        }
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI digitalQueueAPI() {
+
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://9023.pro604cr.amypo.ai");
+
+        Server httpServer = new Server();
+        httpServer.setUrl("http://9023.pro604cr.amypo.ai");
+
+        return new OpenAPI()
+                .servers(List.of(httpsServer, httpServer))
+                .info(new Info()
+                        .title("Digital Queue API")
+                        .version("1.0")
+                        .description("Digital Queue Management System API"));
+    }
 }

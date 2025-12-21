c4c6cb1e5255ad62653c05bc89bc6fb7c5d361ad
bala-amypo <bala@amypo.in>
2025-12-21 13:29:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,11 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Table(name="users")
 public class User {
 
     @Id

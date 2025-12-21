e07d29a218ebe01ebe52f48c4da8f4dc5bd6cc1f
bala-amypo <bala@amypo.in>
2025-12-21 16:06:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/JwtService.java
@@ -0,0 +1,23 @@
+package com.example.demo.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "mysecretkey12345";
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}

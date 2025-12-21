8c6b4013d84038414919ed85966dacb844a193b7
bala-amypo <bala@amypo.in>
2025-12-21 13:22:26 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,27 +4,29 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiSecurityConfig {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Digital Queue Management System")
                         .version("1.0")
                         .description("API Documentation"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecurityScheme("bearerAuth",
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
-                                        .name("Authorization")
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
                                         .type(SecurityScheme.Type.HTTP)
-                                        .in(SecurityScheme.In.HEADER)));
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
     }
 }

5a8dcd3bd65f29227e122157c1fe87b32aaf9c17
bala-amypo <bala@amypo.in>
2025-12-23 08:01:55 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.dto.RegisterRequest;
+import com.example.demo.entity.User;
 
 public interface UserService {
 
-    AuthResponse register(RegisterRequest request);
+    User register(User user);
 
-    AuthResponse login(AuthRequest request);
+    User findByEmail(String email);
+
+    // REQUIRED because your interface already declares it
+    String login(AuthRequest request);
 }

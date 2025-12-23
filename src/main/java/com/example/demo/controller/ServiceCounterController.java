621db44f9ad667f15518819cf1d41527f0581e6e
bala-amypo <bala@amypo.in>
2025-12-23 15:52:40 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServiceCounterController.java
+++ src/main/java/com/example/demo/controller/ServiceCounterController.java
@@ -2,13 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.ServiceCounter;
 import com.example.demo.service.ServiceCounterService;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/counters")
+@Tag(name = "Service Counters")
 public class ServiceCounterController {
 
     private final ServiceCounterService serviceCounterService;
@@ -17,13 +19,15 @@ public class ServiceCounterController {
         this.serviceCounterService = serviceCounterService;
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<ServiceCounter> addCounter(@RequestBody ServiceCounter counter) {
-        return ResponseEntity.ok(serviceCounterService.addCounter(counter));
+    @PostMapping
+    @Operation(summary = "Add service counter")
+    public ServiceCounter addCounter(@RequestBody ServiceCounter counter) {
+        return serviceCounterService.addCounter(counter);
     }
 
     @GetMapping("/active")
-    public ResponseEntity<List<ServiceCounter>> getActiveCounters() {
-        return ResponseEntity.ok(serviceCounterService.getActiveCounters());
+    @Operation(summary = "Get active counters")
+    public List<ServiceCounter> getActiveCounters() {
+        return serviceCounterService.getActiveCounters();
     }
 }

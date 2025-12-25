ffbb94924c5d1bd276afea2d8f5cfc761ceef3d7
bala-amypo <bala@amypo.in>
2025-12-25 05:54:44 +0000

    User commit

--- src/main/java/com/example/demo/service/TokenService.java
+++ src/main/java/com/example/demo/service/TokenService.java
@@ -1,9 +1,20 @@
+// package com.example.demo.service;
+
+// import com.example.demo.entity.Token;
+
+// public interface TokenService {
+//     Token issueToken(Long counterId);
+//     Token updateStatus(Long tokenId, String status);
+//     Token getToken(Long tokenId);
+// }
 package com.example.demo.service;
 
 import com.example.demo.entity.Token;
+import java.util.List;
 
 public interface TokenService {
     Token issueToken(Long counterId);
     Token updateStatus(Long tokenId, String status);
     Token getToken(Long tokenId);
+    List<Token> getTokensByCounter(Long counterId, String status);
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,16 @@
+// package com.example.demo.service;
+
+// import com.example.demo.entity.User;
+
+// public interface UserService {
+//     User registerUser(User user);
+//     User findByEmail(String email);
+// }
 package com.example.demo.service;
 
 import com.example.demo.entity.User;
 
 public interface UserService {
-    User registerUser(User user);
+    User register(User user);
     User findByEmail(String email);
 }

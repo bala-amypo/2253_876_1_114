01b98f9e166e0fa52c93242119ebfbe53403f874
bala-amypo <bala@amypo.in>
2025-12-25 15:01:14 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -31,10 +31,28 @@
 //     }
 // }
 
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Token Management API")
+//                         .version("1.0")
+//                         .description("API for managing tokens and queues"));
+//     }
+// }
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -42,11 +60,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Token Management API")
-                        .version("1.0")
-                        .description("API for managing tokens and queues"));
+    public OpenAPI openAPI() {
+        return new OpenAPI();
     }
 }

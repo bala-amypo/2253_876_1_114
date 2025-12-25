a6cb87527e9f280aed8234e730f6ec120bcf672c
bala-amypo <bala@amypo.in>
2025-12-25 15:31:14 +0000

    User commit

--- src/main/java/com/example/demo/service/TokenService.java
+++ src/main/java/com/example/demo/service/TokenService.java
@@ -8,14 +8,26 @@
 //     Token getToken(Long tokenId);
 // }
 
+// package com.example.demo.service;
+
+// import com.example.demo.entity.Token;
+// import java.util.List;
+
+// public interface TokenService {
+//     Token issueToken(Long counterId);
+//     Token updateStatus(Long tokenId, String status);
+//     Token getToken(Long tokenId);
+//     List<Token> getTokensByCounter(Long counterId, String status);
+// }
 package com.example.demo.service;
 
 import com.example.demo.entity.Token;
-import java.util.List;
 
 public interface TokenService {
+
     Token issueToken(Long counterId);
-    Token updateStatus(Long tokenId, String status);
+
+    Token updateStatus(Long tokenId, String newStatus);
+
     Token getToken(Long tokenId);
-    List<Token> getTokensByCounter(Long counterId, String status);
 }

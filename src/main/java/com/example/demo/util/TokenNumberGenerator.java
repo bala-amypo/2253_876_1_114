39cf38632922bab32b92f450cc2e44b2ce6e9046
bala-amypo <bala@amypo.in>
2025-12-25 06:16:12 +0000

    User commit

--- src/main/java/com/example/demo/util/TokenNumberGenerator.java
+++ src/main/java/com/example/demo/util/TokenNumberGenerator.java
@@ -1,16 +1,16 @@
-package com.example.demo.util;
+// package com.example.demo.util;
 
-import java.util.concurrent.atomic.AtomicInteger;
+// import java.util.concurrent.atomic.AtomicInteger;
 
-public class TokenNumberGenerator {
+// public class TokenNumberGenerator {
 
-    private static final AtomicInteger counter = new AtomicInteger(1000);
+//     private static final AtomicInteger counter = new AtomicInteger(1000);
 
-    private TokenNumberGenerator() {
-        // utility class
-    }
+//     private TokenNumberGenerator() {
+//         // utility class
+//     }
 
-    public static String generateTokenNumber() {
-        return "T" + counter.incrementAndGet();
-    }
-}
+//     public static String generateTokenNumber() {
+//         return "T" + counter.incrementAndGet();
+//     }
+// }

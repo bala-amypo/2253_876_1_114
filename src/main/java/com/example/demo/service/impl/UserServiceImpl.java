d1d6672358c1eaf2337c8aae224ad86ba3e236b1
bala-amypo <bala@amypo.in>
2025-12-26 10:43:14 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -220,6 +220,10 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        // this.passwordEncoder = passwordEncoder;
+    }
     public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;

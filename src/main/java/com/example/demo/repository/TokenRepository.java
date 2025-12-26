4b8842199f27f38d1f91da0acbfbfbfc8fe2dc54
bala-amypo <bala@amypo.in>
2025-12-26 04:25:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/TokenRepository.java
+++ src/main/java/com/example/demo/repository/TokenRepository.java
@@ -30,20 +30,31 @@
 //     List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(Long serviceCounterId, String status);
 // }
 
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.Token;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// public interface TokenRepository extends JpaRepository<Token, Long> {
+
+//     Optional<Token> findByTokenNumber(String tokenNumber);
+
+//     List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(
+//             Long counterId, String status
+//     );
+// }
+
 package com.example.demo.repository;
 
 import com.example.demo.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
-
     Optional<Token> findByTokenNumber(String tokenNumber);
-
-    List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(
-            Long counterId, String status
-    );
+    List<Token> findByServiceCounter_IdAndStatusOrderByIssuedAtAsc(Long counterId, String status);
 }
-
